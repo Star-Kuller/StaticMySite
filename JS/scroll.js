7cf23553f3a21cca0eff39a42dea8c2a18bac777
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2023-10-28 14:33:23 +0300

    added more menu

--- JS/scroll.js
+++ JS/scroll.js
@@ -8,6 +8,9 @@ ScrollSmoother.create({
 
 let originalHeight = 0;
 let originalWidth = 0;
+let profile = document.querySelector('.profile');
+let userRole = document.querySelector('.profile__role');
+let userName = document.querySelector('.profile__username');
 
 const img = new Image();
 img.onload = () => {
@@ -17,7 +20,25 @@ img.onload = () => {
 img.src = '/img/background.png';
 
 $(window).scroll(() => {
-    if (originalWidth === 0) return; // Проверка на деление на ноль
+    if (originalWidth === 0) return;
+
+    if (window.scrollY == 0) {
+        profile.style.height = "12.5vh";
+        profile.style.width = "22.5vw";
+        profile.style.right = "calc(var(--index)*1)";
+        profile.style.padding = "0 0.3vw";
+        userRole.style.fontSize = "calc(var(--index)*0.75)";
+        userRole.style.opacity = "1";
+        userName.style.fontSize = "calc(var(--index)*1)";
+    } else {
+        profile.style.height = "10vh";
+        profile.style.width = "15vw";
+        profile.style.right = "calc(var(--index)*0.5)";
+        profile.style.padding = "0";
+        userRole.style.fontSize = "0";
+        userRole.style.opacity = "0";
+        userName.style.fontSize = "calc(var(--index)*0.75)";
+    }
 
     const cssWidth = $("#background").width();
     const imgHeight = (cssWidth * originalHeight) / originalWidth;
